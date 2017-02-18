e7764e0064bd186e4a68934cf2ed338247d392e3
irtehbom <irtehbom@gmail.com>
2017-02-18 14:07:59 +0000

    Arma update
    Arma update stuff

---
+++ arma_update/server_update.bat
@@ -0,0 +1,7 @@
+@echo off
+echo This Will Install Stable Arma3
+steamcmd +login "irtehbom" "Effort250" +force_install_dir C:\A3Gaming_Server\ +app_update 233780 validate +quit
+echo ----------------------------------------
+echo Download Compleate
+echo ----------------------------------------
+pause

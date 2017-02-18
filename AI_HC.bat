604261fcf44b715bf7ff0a4bbc7767bc3e8b87aa
irtehbom <irtehbom@gmail.com>
2017-02-18 15:02:42 +0000

    AI_HC

--- AI_HC.bat
+++ AI_HC.bat
@@ -1,2 +1,2 @@
 cd /D "C:\Program Files (x86)\Steam\steamapps\common\Arma 3"
-"C:\Program Files (x86)\Steam\steamapps\common\Arma 3\arma3server.exe" "-name=ai_hc" -client -connect=127.0.0.1 -port=2302 -filePatching -mod=@ai_hc;
+"C:\Program Files (x86)\Steam\steamapps\common\Arma 3\arma3server.exe" -client -name=ai_hc -config=C:\HC\config\config_hc.cfg -profiles=C:\HC\profile -connect=193.70.80.115 -port=2302 -mod=@ai_hc;
